90352480ed2ce20bbeea2d2132c2329dfead1ea9
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-08-27 16:02:16 +0100

    Add WhiskyContainer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import WhiskyContainer from "./containers/WhiskyContainer"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey</h1>
+      <WhiskyContainer />
     </div>
   );
 }
---
+++ frontend/src/containers/WhiskyContainer.tsx
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class WhiskyContainer extends Component {
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    render(): any {
+        return (
+            <>
+                <h1>Hey from Container!</h1>
+            </>
+        )
+    }
+
+}
+
+export default WhiskyContainer;
