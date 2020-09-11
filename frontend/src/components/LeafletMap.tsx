3be19ab00397aa32bf87d92e0474fa6a9bed64b9
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-09-11 17:24:39 +0100

    Styled map

--- frontend/src/components/LeafletMap.tsx
+++ frontend/src/components/LeafletMap.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import { Map, TileLayer } from "react-leaflet";
 import { LatLngTuple } from "leaflet";
 
-const defaultLatLng: LatLngTuple = [48.865572, 2.283523];
-const zoom: number = 8;
+const defaultLatLng: LatLngTuple = [56.788845, -4.3396];
+const zoom: number = 7;
 
 interface Props {
     className?: string
@@ -18,6 +18,10 @@ export const LeafletMap: React.FC<Props> = (props) => {
                     attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors">
                 </TileLayer>
             </Map>
+            <a href="https://leafletjs.com/" className="contributor-link">Leaflet</a>
+            <p className="contributor-link">  | &copy;</p>
+            <a href="https://www.openstreetmap.org/copyright" className="contributor-link">OpenStreetMap  </a>
+            <p className="contributor-link"> contributors</p>
         </div>
 
     )
