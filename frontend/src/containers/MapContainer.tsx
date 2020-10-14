799c411f97710f72c654fe37647ba8a1fce3a613
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-10-14 15:41:05 +0100

    Refactor mapcontainer to use hooks

--- frontend/src/containers/MapContainer.tsx
+++ frontend/src/containers/MapContainer.tsx
@@ -1,42 +1,30 @@
-import * as React from "react";
-import { Distillery, Whisky } from "../types/types";
-import { LeafletMap } from "../components/LeafletMap";
-import { DetailedInfo } from "../components/DetailedInfo";
-import { DistilleryPhotos } from "../components/DistilleryPhotos";
+import React, { useState } from "react"
+import { Distillery, Whisky } from "../types/types"
+import { LeafletMap } from "../components/LeafletMap"
+import { DetailedInfo } from "../components/DetailedInfo"
+import { DistilleryPhotos } from "../components/DistilleryPhotos"
 
 interface Props {
-    distilleries?: Distillery[];
+    distilleries?: Distillery[]
+    selected?: Distillery | Whisky
 }
 
-interface State {
-    displayDistilleries: Distillery[];
-    selected?: Distillery | Whisky;
-}
+export const MapContainer: React.FC<Props> = (props) => {
 
-export class MapContainer extends React.Component<Props, State> {
+    const [displayDistilleries, setDisplayDistilleries] = useState<Distillery[]>()
+    const [selected, setSelected] = useState<Distillery | Whisky>()
 
-    state: State = {
-        displayDistilleries: [],
-        selected: undefined
-    }
-
-    componentDidMount() {
-        // Grab three random distilleries and set pics
-    }
-
-    render() {
-        return (
-            <>
-                <div className="map-container-grid">
-                    <div className="grid-1-of-2">
-                        <DetailedInfo className="detailed-info" />        
-                        <DistilleryPhotos className="composition" />
-                    </div>
-                    <div className="map-box">
-                        <LeafletMap className="map"/>
-                    </div>
+    return (
+        <>
+            <div className="map-container-grid">
+                <div className="grid-1-of-2">
+                    <DetailedInfo className="detailed-info" />        
+                    <DistilleryPhotos className="composition" />
                 </div>
-            </>
-        )
-    }
+                <div className="map-box">
+                    <LeafletMap className="map"/>
+                </div>
+            </div>
+        </>
+    )
 }
